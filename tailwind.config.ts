a58820abf82490549d5bbc86c3d246ac4a9a1364
Alejo <alejoczombos@gmail.com>
2024-04-10 10:58:01 -0300

    config tailwind colors variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,48 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'primary': {
+        '50': '#fdf6f3',
+        '100': '#fceae4',
+        '200': '#fad9ce',
+        '300': '#f6beab',
+        '400': '#ec8c6b',
+        '500': '#e27651',
+        '600': '#cf5b33',
+        '700': '#ad4a28',
+        '800': '#904024',
+        '900': '#783924',
+        '950': '#411b0e',
+        },
+        'secondary': {
+            '50': '#f5f6f6',
+            '100': '#e4e6e9',
+            '200': '#cdd2d4',
+            '300': '#aab1b6',
+            '400': '#7f8991',
+            '500': '#5e676f',
+            '600': '#565d64',
+            '700': '#494e55',
+            '800': '#414449',
+            '900': '#393c40',
+            '950': '#232529',
+        },
+        'tertiary': {
+            '50': '#f6f7f8',
+            '100': '#ebecee',
+            '200': '#dcdee1',
+            '300': '#c9cdd0',
+            '400': '#a7adb3',
+            '500': '#92989f',
+            '600': '#81868f',
+            '700': '#747881',
+            '800': '#61656c',
+            '900': '#505358',
+            '950': '#333438',
+        },
+      }
+
     },
   },
   plugins: [],
