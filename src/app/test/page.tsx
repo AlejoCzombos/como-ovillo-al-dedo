61d662bc1e9a1400f26881fa8e4bfb968119de91
Alejo <alejoczombos@gmail.com>
2024-04-10 11:59:36 -0300

    testeo de qrReader component

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import QrReader from "@/components/QrReader";
+
+export default function () {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <QrReader />
+    </div>
+  );
+}
