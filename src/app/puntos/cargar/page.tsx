28361bda28e01cdc2c2592c0460896f6b188cdcd
Alejo <alejoczombos@gmail.com>
2024-04-14 23:19:14 -0300

    Añadir funcionalidad de carga de puntos a la página principal

--- src/app/puntos/cargar/page.tsx
+++ src/app/puntos/cargar/page.tsx
@@ -1,19 +1,70 @@
 "use client";
 
+import ModalIdClient from "@/components/ModalIdClient";
+import ModalQr from "@/components/ModalQr";
+import BigButton from "@/components/BigButton";
 import { qrState, useQr } from "@/lib/store/Qr";
+import { useState } from "react";
 
 export default function CargarPuntos() {
-  const { result } = useQr() as qrState;
+  const { result, setIsOpen, isOpen } = useQr() as qrState;
+  const [idClientIsOpen, setIdClientIsOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const totalSale = form.totalSale.value;
+    const res = await fetch("/api/clientes/puntos/cargar", {
+      method: "POST",
+      body: JSON.stringify({ monto: totalSale, cliente_id: result }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      alert(`Puntos cargados: ${data.puntosActuales}`);
+    }
+  };
+
   return (
-    <main className="flex justify-center min-h-full h-screen w-full m-auto">
-      <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5">
-        <button className="bg-tertiary-200 text-secondary-500 py-4 text-3xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out">
-          ESCANEAR CON QR
-        </button>
-        <button className="bg-tertiary-200 text-secondary-500 py-4 text-3xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out">
-          CARGAR POR NUMERO DE CLIENTE
-        </button>
-      </section>
+    <main className="flex justify-center min-h-[100vw] w-full m-auto">
+      {!result && (
+        <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5">
+          <BigButton text="ESCANEAR CON QR" onClick={handleClick} />
+          <BigButton
+            text="CARGAR POR NUMERO DE CLIENTE"
+            onClick={() => setIdClientIsOpen(true)}
+          />
+        </section>
+      )}
+      {result && (
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5"
+        >
+          <div className="w-full p-2 text-2xl">
+            <label className="text-white" htmlFor="totalSale">
+              Monto de la venta:
+            </label>
+            <input
+              type="number"
+              id="totalSale"
+              className="w-full p-2 bg-secondary-100 rounded-xl"
+            />
+          </div>
+          <BigButton text="CARGAR PUNTOS" />
+        </form>
+      )}
+      {isOpen && <ModalQr />}
+      {idClientIsOpen && (
+        <ModalIdClient isOpen={idClientIsOpen} setIsOpen={setIdClientIsOpen} />
+      )}
     </main>
   );
 }
