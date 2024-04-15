236d61b3605003f7e584fcce4bf7fddc71a14f96
Alejo <alejoczombos@gmail.com>
2024-04-14 23:19:09 -0300

    Añadir componente BigButton a la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
+import BigButton from "@/components/BigButton";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-center min-h-full h-screen w-full m-auto">
+    <main className="flex justify-center min-h-[100vw] w-full m-auto">
       <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md">
         <Link href="/puntos/cargar" className="w-full">
-          <button className="bg-tertiary-200 text-secondary-500 py-4 text-3xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out">
-            CARGAR PUNTOS
-          </button>
+          <BigButton text="CARGAR PUNTOS" />
         </Link>
         <Link href="/puntos/ver" className="w-full">
-          <button className="bg-tertiary-200 text-secondary-500 py-4 text-3xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out">
-            VER PUNTOS
-          </button>
+          <BigButton text="VER PUNTOS" />
         </Link>
         <Link href="/puntos/canjear" className="w-full">
-          <button className="bg-tertiary-200 text-secondary-500 py-4 text-3xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out">
-            CANJEAR PUNTOS
-          </button>
+          <BigButton text="CANJEAR PUNTOS" />
         </Link>
       </section>
     </main>
