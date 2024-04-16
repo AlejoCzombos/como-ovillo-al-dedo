5368af9daca5ab9fa2a29b4f9de960e4e3009ae1
Alejo <alejoczombos@gmail.com>
2024-04-16 20:46:25 -0300

    mensaje al cargar puntos

--- src/app/puntos/cargar/page.tsx
+++ src/app/puntos/cargar/page.tsx
@@ -5,9 +5,10 @@ import ModalQr from "@/components/ModalQr";
 import BigButton from "@/components/BigButton";
 import { qrState, useQr } from "@/lib/store/Qr";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function CargarPuntos() {
-  const { result, setIsOpen, isOpen } = useQr() as qrState;
+  const { result, setIsOpen, isOpen, setResult } = useQr() as qrState;
   const [idClientIsOpen, setIdClientIsOpen] = useState<boolean>(false);
 
   const handleClick = () => {
@@ -18,7 +19,8 @@ export default function CargarPuntos() {
     e.preventDefault();
     const form = e.currentTarget;
     const totalSale = form.totalSale.value;
-    const res = await fetch("/api/clientes/puntos/cargar", {
+
+    const data = fetch("/api/clientes/puntos/cargar", {
       method: "POST",
       body: JSON.stringify({ monto: totalSale, cliente_id: result }),
       headers: {
@@ -26,16 +28,27 @@ export default function CargarPuntos() {
       },
     });
 
-    if (res.ok) {
-      const data = await res.json();
-      alert(`Puntos cargados: ${data.puntosActuales}`);
-    }
+    toast.promise(
+      data.then((res) => res.json()),
+      {
+        loading: "Cargando puntos...",
+        success: (data) =>
+          `Puntos cargados\nPuntos actuales: ${data.puntosActuales}`,
+        error: "Error al cargar los puntos",
+      }
+    );
+
+    setResult("");
+    form.reset();
   };
 
   return (
     <main className="flex justify-center min-h-[100vw] w-full m-auto">
       {!result && (
-        <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5">
+        <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5 sm:px-0 relative">
+          <h2 className="text-3xl text-center w-[80%] text-white font-semibold absolute top-20 left-1/2 transform -translate-x-1/2">
+            Carga de Puntos
+          </h2>
           <BigButton text="ESCANEAR CON QR" onClick={handleClick} />
           <BigButton
             text="CARGAR POR NUMERO DE CLIENTE"
@@ -46,7 +59,7 @@ export default function CargarPuntos() {
       {result && (
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5"
+          className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5 sm:px-0"
         >
           <div className="w-full p-2 text-2xl">
             <label className="text-white" htmlFor="totalSale">
