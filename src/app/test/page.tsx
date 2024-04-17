96a5b06ad2c03f61c71c3fb30fa05a52b8736bcf
Alejo <alejoczombos@gmail.com>
2024-04-16 23:39:41 -0300

    Eliminar página de prueba de QrReader

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import QrReader from "@/components/QrReader";
-
-export default function Test() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <QrReader />
-    </div>
-  );
-}
