5e2c3119794ff9abaa2b33a7d625fa995aede5ad
Alejo <alejoczombos@gmail.com>
2024-04-17 21:44:57 -0300

    cambio de rutas canjear y cargar puntos

--- src/app/admin/canjear-puntos/page.tsx
+++ src/app/admin/canjear-puntos/page.tsx
@@ -7,7 +7,7 @@ import { qrState, useQr } from "@/lib/store/Qr";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
-export default function CargarPuntos() {
+export default function CanjearPuntos() {
   const { result, setIsOpen, isOpen, setResult } = useQr() as qrState;
   const [idClientIsOpen, setIdClientIsOpen] = useState<boolean>(false);
 
@@ -18,15 +18,15 @@ export default function CargarPuntos() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget;
-    const totalSale = form.totalSale.value;
+    const totalPoints = form.totalPoints.value;
     const password = form.password.value;
 
     const toastPromise = toast.loading("Cargando puntos...");
     const data = await fetch(
-      `/api/clientes/puntos/cargar?password=${password}`,
+      `/api/clientes/puntos/canjear?password=${password}`,
       {
         method: "POST",
-        body: JSON.stringify({ monto: totalSale, cliente_id: result }),
+        body: JSON.stringify({ puntos: totalPoints, cliente_id: result }),
         headers: {
           "Content-Type": "application/json",
         },
@@ -36,11 +36,13 @@ export default function CargarPuntos() {
     const response = await data.json();
     if (data.status === 200) {
       toast.success(
-        `Puntos cargados\nPuntos totales: ${response.puntosActuales}`,
+        `Puntos cargados\nPuntos restantes: ${response.puntosTotales}`,
         {
           id: toastPromise,
         }
       );
+    } else if (data.status === 400) {
+      toast.error("No hay suficientes puntos", { id: toastPromise });
     } else if (data.status === 401) {
       toast.error("Contraseña incorrecta", { id: toastPromise });
     } else if (data.status === 404) {
@@ -54,15 +56,15 @@ export default function CargarPuntos() {
   };
 
   return (
-    <main className="flex justify-center min-h-[100vw] w-full m-auto">
+    <main className="flex justify-center min-h-[85vh] w-full m-auto relative">
+      <h2 className="text-4xl text-center w-[80%] text-white font-semibold absolute top-20 left-1/2 transform -translate-x-1/2">
+        Canjeo de Puntos
+      </h2>
       {!result && (
-        <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5 sm:px-0 relative">
-          <h2 className="text-3xl text-center w-[80%] text-white font-semibold absolute top-20 left-1/2 transform -translate-x-1/2">
-            Carga de Puntos
-          </h2>
+        <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5 sm:px-0">
           <BigButton text="ESCANEAR CON QR" onClick={handleClick} />
           <BigButton
-            text="CARGAR POR NUMERO DE CLIENTE"
+            text="CANJEAR POR NUMERO DE CLIENTE"
             onClick={() => setIdClientIsOpen(true)}
           />
         </section>
@@ -73,12 +75,12 @@ export default function CargarPuntos() {
           className="flex flex-col items-center justify-center gap-2 w-full max-w-md px-5 sm:px-0"
         >
           <div className="w-full text-2xl">
-            <label className="text-white" htmlFor="totalSale">
-              Monto de la venta:
+            <label className="text-white" htmlFor="totalPoints">
+              Puntos a canjear:
             </label>
             <input
               type="number"
-              id="totalSale"
+              id="totalPoints"
               className="w-full p-2 bg-secondary-100 rounded-xl"
             />
           </div>
@@ -92,7 +94,7 @@ export default function CargarPuntos() {
               className="w-full p-2 bg-secondary-100 rounded-xl"
             />
           </div>
-          <BigButton text="CARGAR PUNTOS" />
+          <BigButton text="CANJEAR PUNTOS" />
         </form>
       )}
       {isOpen && <ModalQr />}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-center min-h-[100vw] w-full m-auto">
+    <main className="flex justify-center min-h-[85vh] w-full m-auto">
       <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5 sm:px-0">
-        <Link href="/puntos/cargar" className="w-full">
-          <BigButton text="CARGAR PUNTOS" />
-        </Link>
         <Link href="/puntos/ver" className="w-full">
-          <BigButton text="VER PUNTOS" />
-        </Link>
-        <Link href="/puntos/canjear" className="w-full">
-          <BigButton text="CANJEAR PUNTOS" />
+          <BigButton text="CONSULTAR PUNTOS" />
         </Link>
       </section>
     </main>
