dce7e4d0da22198a49734d6c1758a769d883b295
Alejo <alejoczombos@gmail.com>
2024-04-20 10:30:10 -0300

    componetización de cada componente de formulario e integracion con validaciones

---
+++ src/components/Input.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useFormContext, Controller } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  name: string;
+  type?: string;
+  password?: boolean;
+  rules?: any;
+}
+
+const Input: React.FC<InputProps> = ({
+  label,
+  name,
+  type = "text",
+  password = false,
+  rules,
+}) => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="w-full text-2xl">
+      <label className="text-white" htmlFor={name}>
+        {label}:
+      </label>
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field, fieldState }) => (
+          <>
+            <input
+              {...field}
+              type={type}
+              id={name}
+              className={`w-full p-2 bg-secondary-100 rounded-xl ${
+                password ? "bg-secondary-200 border-white border-2" : ""
+              } ${fieldState.error ? "border-red-500" : ""}`}
+            />
+            {fieldState.error && (
+              <span className="text-red-500 text-lg">
+                {fieldState.error.message}
+              </span>
+            )}
+          </>
+        )}
+      />
+    </div>
+  );
+};
+
+export default Input;
