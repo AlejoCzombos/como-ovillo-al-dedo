880b33e2f5601b1a0002f607bed7b2a5f733865f
Alejo <alejoczombos@gmail.com>
2024-04-20 11:54:15 -0300

    añadir prop de className al botón

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -1,14 +1,19 @@
 export default function BigButton({
   text,
+  className,
   ...props
 }: {
   text: string;
+  className?: string;
   [key: string]: any;
 }) {
   return (
     <button
       {...props}
-      className="bg-tertiary-200 text-secondary-500 py-4 text-pretty text-2xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out"
+      className={
+        `bg-tertiary-200 text-secondary-500 py-4 text-pretty text-2xl w-full font-semibold drop-shadow-lg rounded-3xl hover:bg-tertiary-100 transition-colors ease-in-out ` +
+        className
+      }
     >
       {text}
     </button>
