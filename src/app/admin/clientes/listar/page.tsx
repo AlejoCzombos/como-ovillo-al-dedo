aa829fdd24c8be4fa97d3c8e7e906c9e225aa2bd
Alejo <alejoczombos@gmail.com>
2024-04-20 10:30:24 -0300

    ruta de listar clientes

---
+++ src/app/admin/clientes/listar/page.tsx
@@ -0,0 +1,9 @@
+export default function ListarClientes() {
+  return (
+    <main className="min-h-[85vh] w-full m-auto py-5">
+      <h2 className="text-4xl text-center w-[80%] text-white font-semibold m-auto py-5">
+        Modificar Cliente
+      </h2>
+    </main>
+  );
+}
