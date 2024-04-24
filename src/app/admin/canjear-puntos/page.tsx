8ce744063ac866b862a9fcf2bd212682ac825a83
Alejo <alejoczombos@gmail.com>
2024-04-23 21:50:44 -0300

    Agregar estado y respuesta de carga de puntos en CanjearPuntos

--- src/app/admin/canjear-puntos/page.tsx
+++ src/app/admin/canjear-puntos/page.tsx
@@ -11,6 +11,9 @@ import UpdatePointsForm from "@/components/UpdatePointForm";
 export default function CanjearPuntos() {
   const { result, setIsOpen, isOpen, setResult } = useQr() as qrState;
   const [idClientIsOpen, setIdClientIsOpen] = useState<boolean>(false);
+  const [wasCharged, setWasCharged] = useState<boolean>(false);
+  const [updatePointsResponse, setUpdatePointsResponse] =
+    useState<UpdatePointsResponse>();
 
   const handleClick = () => {
     setIsOpen(true);
@@ -33,12 +36,9 @@ export default function CanjearPuntos() {
 
     const response = await data.json();
     if (data.status === 200) {
-      toast.success(
-        `Puntos cargados\nPuntos restantes: ${response.puntosTotales}`,
-        {
-          id: toastPromise,
-        }
-      );
+      toast.remove(toastPromise);
+      setUpdatePointsResponse(response);
+      setWasCharged(true);
     } else if (data.status === 400) {
       toast.error("No hay suficientes puntos", { id: toastPromise });
     } else if (data.status === 401) {
@@ -54,10 +54,12 @@ export default function CanjearPuntos() {
 
   return (
     <main className="flex justify-center min-h-[85vh] w-full m-auto relative">
-      <h2 className="text-4xl text-center w-[80%] text-white font-semibold absolute top-20 left-1/2 transform -translate-x-1/2">
-        Canjear Puntos
-      </h2>
-      {!result && (
+      {!wasCharged && (
+        <h2 className="text-4xl text-center w-[80%] text-white font-semibold absolute top-20 left-1/2 transform -translate-x-1/2">
+          Canjear Puntos
+        </h2>
+      )}
+      {!result && !wasCharged && (
         <section className="flex flex-col items-center justify-center gap-8 w-full max-w-md px-5 sm:px-0">
           <BigButton text="ESCANEAR CON QR" onClick={handleClick} />
           <BigButton
@@ -71,7 +73,24 @@ export default function CanjearPuntos() {
           labelTitule="Puntos"
           buttonLabel="CANJEAR PUNTOS"
           onSubmit={handleSubmit}
+          isCash={false}
         />
+      )}
+      {wasCharged && !result && (
+        <section className="flex flex-col items-center justify-center w-full max-w-md px-5 sm:px-0">
+          <p className="text-white font-semibold text-4xl text-center mb-2">
+            ¡Puntos canjeados correctamente!
+          </p>
+          <p className="text-white font-semibold text-2xl">
+            <span className="text-secondary-500">
+              {updatePointsResponse?.name}
+            </span>{" "}
+            ahora tiene:
+          </p>
+          <p className="text-secondary-500 text-4xl font-semibold">
+            {updatePointsResponse?.currentPoints} puntos
+          </p>
+        </section>
       )}
       {isOpen && <ModalQr />}
       {idClientIsOpen && (
