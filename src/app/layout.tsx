1111f953727f0671be2f20957517101707fd3245
Alejo <alejoczombos@gmail.com>
2024-04-23 21:50:35 -0300

    agregar favicon al layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <link rel="icon" href="/favicon.png" sizes="any" />
       <body className={typography.className}>
         <Navigation />
         {children}
