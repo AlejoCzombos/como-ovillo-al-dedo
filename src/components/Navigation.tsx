ef4728af7e5c7994e00b0bf199ac5b1d0ac44f32
Alejo <alejoczombos@gmail.com>
2024-05-12 12:15:41 -0300

    otras cosas

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,51 +10,51 @@ import { browserSessionPersistence, onAuthStateChanged, setPersistence } from "f
 import { auth } from "@/lib/firebase/firebase";
 
 export default function Navigation() {
-    const pathName = usePathname();
-    const route = useRouter();
-    const { setResult } = useQr() as qrState;
+  const pathName = usePathname();
+  const route = useRouter();
+  const { setResult } = useQr() as qrState;
 
-    const handleGoBack = () => {
-        setResult("");
-        route.back();
-    };
+  const handleGoBack = () => {
+    setResult("");
+    route.back();
+  };
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setPersistence(auth, browserSessionPersistence);
-            } else {
-                console.log("user is logged out");
-            }
-        });
-    }, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPersistence(auth, browserSessionPersistence);
+      } else {
+        console.log("user is logged out");
+      }
+    });
+  }, []);
 
-    return (
-        <nav className="sticky top-0 z-20 w-full shadow-md bg-white h-24 ">
-            <div className="flex h-full max-w-6xl items-center justify-start">
-                {pathName !== "/" && (
-                    <button onClick={handleGoBack}>
-                        <img
-                            src="/BackIcon.svg"
-                            alt="Volver al menu"
-                            className="size-10 -rotate-90 cursor-pointer hover:scale-125 transform transition-transform ease-in-out duration-300"
-                        />
-                    </button>
-                )}
-                <Link
-                    href="/"
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                >
-                    <Image
-                        priority={true}
-                        width={750}
-                        height={300}
-                        className="size-24 object-contain w-auto sm:h-20 sm:w-auto"
-                        src={logo}
-                        alt="Logo de 101Patitas"
-                    />
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="sticky top-0 z-20 w-full shadow-md bg-white h-24 ">
+      <div className="flex h-full max-w-6xl items-center justify-start">
+        {pathName !== "/" && (
+          <button onClick={handleGoBack}>
+            <img
+              src="/BackIcon.svg"
+              alt="Volver al menu"
+              className="size-10 -rotate-90 cursor-pointer hover:scale-125 transform transition-transform ease-in-out duration-300"
+            />
+          </button>
+        )}
+        <Link
+          href="/"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        >
+          <Image
+            priority={true}
+            width={750}
+            height={300}
+            className="size-24 object-contain w-auto sm:h-20 sm:w-auto"
+            src={logo}
+            alt="Logo de 101Patitas"
+          />
+        </Link>
+      </div>
+    </nav>
+  );
 }
