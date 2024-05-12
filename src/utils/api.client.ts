9bf3273a4d8d6615674255c88b18800b8f590271
Alejo <alejoczombos@gmail.com>
2024-05-12 12:10:53 -0300

    api utilizando token en el header para autorización

--- src/utils/api.client.ts
+++ src/utils/api.client.ts
@@ -1,14 +1,15 @@
 export const updateClient = async (
   clientId: number,
-  password: string,
+  token: string,
   clientBody: any
 ) => {
   const response = await fetch(
-    `/api/clientes/${clientId}?password=${password}`,
+    `/api/clientes/${clientId}`,
     {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(clientBody),
     }
@@ -16,11 +17,32 @@ export const updateClient = async (
   return response;
 };
 
-export const createClient = async (password: string, clientBody: any) => {
-  const response = await fetch(`/api/clientes?password=${password}`, {
+export const getClientComplete = async (clientId: number, token: string) => {
+  const response = await fetch(`/api/clientes/${clientId}/completo`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response;
+}
+
+export const getClients = async (token: string) => {
+  const response = await fetch(`/api/clientes`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response;
+};
+
+export const createClient = async (token: string, clientBody: any) => {
+  const response = await fetch(`/api/clientes`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(clientBody),
   });
